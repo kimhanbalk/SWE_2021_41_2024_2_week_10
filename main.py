488f74b31d07766cefd6e6113cd9285956712109
kimhanbalk <kimhanbalk@g.skku.edu>
2024-11-06 19:57:13 +0900

    Add even_list function in skeleton code

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def even_list(int_list: List[int]) -> List[int]:
         A list of even integers.
     """
     # TODO: Implement even_list
+    return [num for num in int_list if num % 2 == 0]
+
     pass
     
 # Skeleton code for sum_of_squares_of_even
